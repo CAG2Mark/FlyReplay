29d08aba0fb348b0f99d87513bc3f58967ea2066
CAG2Mark <markng989@gmail.com>
2021-02-12 18:18:16 +0800

    Skeleton of application done

--- FlyReplay/App.xaml.cs
+++ FlyReplay/App.xaml.cs
@@ -11,5 +11,10 @@ namespace FlyReplay {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            if (SingleInstance.AlreadyRunning())
+                App.Current.Shutdown(); // Just shutdown the current application,if any instance found
+            base.OnStartup(e);
+        }
     }
 }
